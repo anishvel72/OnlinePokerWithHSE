e02c793a3aab855976a657c32b1233732a67c8d9
Anish Velagapudi <velagapudi26a@ncssm.edu>
2025-11-22 21:19:08 -0500

    Google Authentication added (Yet to add user management)

--- server.py
+++ server.py
@@ -1,10 +1,27 @@
-from flask import Flask, redirect, request
+from flask import Flask, redirect, request, render_template, url_for
+from authlib.integrations.flask_client import OAuth
+
+import config
+
+
 
 app = Flask(__name__)
+app.secret_key = 'temporary'
+oauth = OAuth(app)
 
 sessions = {}
 games = {}
 
+CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+google = oauth.register(
+    name = 'google',
+    client_id = config.CLIENT_ID,
+    client_secret = config.CLIENT_SECRET,
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={'scope': 'openid profile email'}
+)
+
+#I want login to be the default route
 @app.route('/')
 def entryPage():
     return redirect('/login')
@@ -15,7 +32,7 @@ def loginPage():
     #If valid session cookie, redirect to dashboard
     #Otherwise, stay
     
-    return "Login Page Placeholder"
+    return render_template('./loginPage.html')
 
 
 @app.route('/dashboard')
@@ -23,6 +40,16 @@ def homePage():
     #If not valid session cookie, redirect to login
     return "Dashboard Page Placeholder"
 
+
+@app.route('/start-google-login')
+def start_google_login():
+    return google.authorize_redirect(url_for('authorize', _external=True), prompt='select_account')
+
+@app.route('/authorize')
+def authorize():
+    token = oauth.google.authorize_access_token()
+    print(token)
+    return redirect('/dashboard')
 
 @app.route('/poker')
 def pokerGame():
