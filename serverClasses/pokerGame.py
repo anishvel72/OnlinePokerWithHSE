b16fda394d76eb84e11be8b91e8b2a0bcae399bb
Anish Velagapudi <velagapudi26a@ncssm.edu>
2025-12-03 08:58:16 -0500

    Updated PokerGame

--- serverClasses/pokerGame.py
+++ serverClasses/pokerGame.py
@@ -1,6 +1,29 @@
 from deck import Deck
 from card import Card
+import uuid
 
-class Poker:
+class PokerGame:
     def __init__(self):
-        pass
+        self._gameID = str(uuid.uuid4())
+        self._river = []
+        self._players = []
+        self._pot = 0
+        self._deck = Deck()
+        self._deck.shuffle()
+    
+    def initialDeal(self):
+        for player in self._players:
+            player.addToHand(self._deck)
+    
+
+    def endRound(self):
+        for player in self._players:
+            player.clearHand()
+        self._river = []
+        self._deck = Deck()
+        self._deck.shuffle()
+
+
+    @property
+    def gameID(self):
+        return self._gameID
